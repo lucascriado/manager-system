5197d049fc1e4145f13b05fa03c59a91fe5ddc93
lucascriado <lucascriado33@gmail.com>
2024-04-20 12:09:46 -0300

    feat: adicionado tela de login e registro

--- script.py
+++ script.py
@@ -11,9 +11,44 @@ class Product(db.Model):
     price = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(120), nullable=False)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))   
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User(name=name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("home"))
+    else:
+        return render_template("register.html")
+    
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password == password:
+                return redirect(url_for("home"))
+            else:
+                return "Password is incorrect"
+        else:
+            return "User not found"
+    else:
+        return render_template("login.html")
 
 @app.route("/home", methods=["POST", "GET"])
 def home():
